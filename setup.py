ef88b3a0a9bdca3c41cabd9b4534e59d3a3e1151
Hao Ding <49543594+0xWelt@users.noreply.github.com>
2024-11-18 03:20:15 +0000

    update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 import os
-import sys
 import codecs
 from setuptools import setup, find_packages
 
@@ -20,33 +19,21 @@ def get_version(rel_path):
         raise RuntimeError("Unable to find version string.")
 
 
-def is_docker():
-    if os.path.exists('/proc/self/cgroup'):
-        with open('/proc/self/cgroup', 'rt') as ifh:
-            return 'docker' in ifh.read()
-    return False
-
-
-def parse_requirements(filename):
-    """ load requirements from a pip requirements file. (replacing from pip.req import parse_requirements)"""
-    lineiter = (line.strip() for line in open(filename))
-    reqs = [line for line in lineiter if line and not line.startswith("#")]
-    if sys.platform == "win32":
-        reqs.append('pywin32')
-    # if py<=3.6 add dataclasses
-    if sys.version_info.major == 3 and sys.version_info.minor <= 6:
-        reqs.append("dataclasses")
-    if sys.version_info.major == 3 and sys.version_info.minor <= 7:
-        reqs.remove("facebook-wda>=1.3.3")
-        reqs.append("facebook-wda<1.4.8")
-    if is_docker():
-        reqs.remove("opencv-contrib-python>=4.4.0.46, <=4.6.0.66")
-        reqs.append("opencv-contrib-python-headless==4.5.5.64")
-    return reqs
+def _fetch_requirements(path: str) -> list:
+    requirements = []
+    with open(path) as f:
+        for r in f:
+            r = r.strip()
+            if r.startswith('-r'):
+                assert len(r.split()) == 2
+                requirements.extend(_fetch_requirements(r.split()[-1]))
+            else:
+                requirements.append(r)
+    return requirements
 
 
 setup(
-    name='airtest',
+    name='airtest-openwsgr',
     version=get_version("airtest/utils/version.py"),
     author='Netease Games',
     author_email='rockywhisper@163.com',
@@ -62,7 +49,7 @@ setup(
         'ios_deps': ["airtest/core/ios/iproxy"],
     },
     include_package_data=True,
-    install_requires=parse_requirements('requirements.txt'),
+    install_requires=_fetch_requirements('requirements.txt'),
     extras_require={
         'tests': [
             'nose',
@@ -78,12 +65,8 @@ setup(
     airtest = airtest.cli.__main__:main
     """,
     classifiers=[
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
     ],
 )
